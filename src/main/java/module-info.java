279abebacbde9c09ec6156c2b43e1874324e1b72
Leif Mørstad <leif@moerstad.no>
2025-04-09 15:40:25 +0200

    modified pom

--- src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-open module notetaker {
-    requires javafx.base;
-    requires javafx.controls;
-    requires javafx.fxml;
-    requires javafx.graphics;
-    requires org.jetbrains.annotations;
-    requires org.junit.jupiter.api;
-}
